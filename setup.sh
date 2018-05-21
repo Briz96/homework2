0466a361329ce1ed93c03efd83cc7879a1348148
cristian filippini <id598ult@studenti.univr.it>
2018-05-21 09:21:47 +0200

    setup aggiornato

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 cp LaboratorioAlfa.world ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/.
 cp homework2_LabAlfa.launch ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/launch/.
-cp -r *(nome della cartella dei modelli)* ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/.
+cp -r Modelli ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/.
 
-echo Select your turtlebot model
+echo Per eseguire le simulazioni utilizzare i seguenti comandi:
 echo export TURTLEBOT3_MODEL=waffle
 
-echo Launch cyber_lab with turtlebot:
-echo roslaunch turtlebot3_gazebo turtlebot3_cyber_lab.launch
+echo Lanciare con:
+echo roslaunch turtlebot3_gazebo homework2_LabAlfa.launch
 
 echo 
-echo Then launch Turtlebot autonomous navigation:
+echo Per la navigazione autonoma:
 echo roslaunch turtlebot3_gazebo turtlebot3_simulation.launch
