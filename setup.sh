d3d367b7e7e1b26d7977bbbbe7a1e7b59bea01a5
Fabrizio <id102blq@studenti.univr.it>
2018-05-21 17:44:59 +0200

    ultimi controlli e modifiche

--- setup.sh
+++ setup.sh
@@ -1,33 +1,32 @@
-cp LaboratorioAlfa.world  /catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/worlds/.
-cp LabAlfa.launch  /catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/launch/.
+cp LaboratorioAlfa.world  //catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/worlds/.
+cp LabAlfa.launch  //catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/launch/.
 
-cp -r Modelli ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/.
+cp -r Modelli //catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/.
 
-cp -r Modelli ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/.
+cp -r Modelli //catkin_ws/src/turtlebot3_simulations/turtlebot3_gazebo/models/.
 
 
 echo La mappa si apre con i comandi:
-echo (aprire un nuovo terminale per ogni coppia di comandi) 
+echo aprire un nuovo terminale per ogni coppia di comandi 
 echo $ export TURTLEBOT3_MODEL=waffle
 echo $ roslaunch turtlebot3_gazebo LabAlfa.launch
-
+echo
 echo Per la teleoperazione in nuovo terminale i comandi:
 echo $ roslaunch turtlebot3_teleop turtlebot3_teleop_key.launch
-
+echo
 echo Per la guida autonoma:
 echo $ export TURTLEBOT3_MODEL=waffle
 echo $ roslaunch turtlebot3_gazebo turtlebot3_simulation.launch
-
+echo
 echo Comandi per eseguire la navigazione nella mappa creata dal turtlebot
-
+echo
 echo Aprire la mappa: 
 echo $ export TURTLEBOT3_MODEL=waffle
 echo $ roslaunch turtlebot3_gazebo LabAlfa.launch
-
+echo
 echo lanciare la mappa:
 echo $ export TURTLEBOT3_MODEL=waffle
-echo $ roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=$/catkin_ws/src/homework2/LabAlfa.pgm
-
+echo $ roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=//catkin_ws/src/homework2/LabAlfa.yaml
 echo Eseguire Rviz:
 echo $ export TURTLEBOT3_MODEL=waffle 
 echo $ rosrun rviz rviz -d `rospack find turtlebot3_slam`/rviz/turtlebot3_slam.rviz
